7c42b4d3b4027b433fcb4d8a229ce4fbcc1edfe2
fqah <Fqah@fqahs-macbook-pro.local>
2015-11-30 17:27:26 +0800

    Initial Commit

---
+++ GGLinkCall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGLinkCall
+//
+//  Created by fqah on 11/30/15.
+//  Copyright © 2015 fqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GGLinkCall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GGLinkCall
+//
+//  Created by fqah on 11/30/15.
+//  Copyright © 2015 fqah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
