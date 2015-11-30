5c0ffc4e51fccd4652bdb4508accfb684b68c9ff
fqah <Fqah@fqahs-MacBook-Pro.local>
2015-11-30 17:40:15 +0800

    first commit

---
+++ GGLinkCall/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GGLinkCall
+//
+//  Created by fqah on 11/30/15.
+//  Copyright © 2015 fqah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GGLinkCall/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GGLinkCall
+//
+//  Created by fqah on 11/30/15.
+//  Copyright © 2015 fqah. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
